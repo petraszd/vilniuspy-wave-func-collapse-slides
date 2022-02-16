5fd0b3c669f1ed2030849960f49d8aa43fa85ad9
Petras Zdanavičius <petraszd@gmail.com>
2022-02-17 00:41:39 +0200

    Groups placement

--- Scripts/WFCCollapser.gd
+++ Scripts/WFCCollapser.gd
@@ -2,40 +2,60 @@ class_name WFCCollapser
 extends Node2D
 
 
+export(int) var padding = 20
+
 export(int) var num_cols = 1
 export(int) var num_rows = 1
 
 var tile_scene = preload("res://Scenes/WFCTile.tscn")
 var tiles = []
 
-func _ready():
-    print("Collapser: ready")
-    generate_tiles()
 
+func _ready():
+    assert(num_cols > 0)
+    assert(num_rows > 0)
+    generate_tiles()
+    rescale_groups_wrapper()
 
 func generate_tiles():
-    var screen_size = get_viewport().size
-    print(screen_size)
-
-    # TODO: Wrong. It depends on a screen size and on a num_cols, num_rows
-    var w = screen_size.x
-    var h = screen_size.y
-
-    var x_delta = 0
-    var y_delta = 0
-    var x_size = min(w, h) / float(num_cols)
-    var y_size = min(w, h) / float(num_rows)
-
-    if h > w:
-        y_delta = (h - w) / 2.0
-    elif w > h:
-        x_delta = (w - h) / 2.0
-
     for y in range(num_rows):
         for x in range(num_cols):
             var tile = tile_scene.instance()
-            tile.idx = y * num_cols + x
-            tile.global_position.x = x_delta + x * x_size
-            tile.global_position.y = y_delta + y * y_size
             $Groups.add_child(tile)
+            tile.idx = y * num_cols + x
+            tile.position.x = x
+            tile.position.y = y
             tiles.append(tile)
+
+func rescale_groups_wrapper():
+    var screen_size = get_viewport().size
+    if screen_size.x == 0 or screen_size.y == 0:
+        return
+
+    var screen_x = screen_size.x - padding * 2
+    if screen_x < padding:
+        screen_x = screen_size.x
+
+    var screen_y = screen_size.y - padding * 2
+    if screen_y < padding:
+        screen_y = screen_size.y
+
+    # TODO: screen size padding
+
+    var x_ratio = screen_x / num_cols
+    var y_ratio = screen_y / num_rows
+    var ratio = min(x_ratio, y_ratio)
+
+    var x_delta = padding
+    var y_delta = padding
+
+    if x_ratio < y_ratio:
+        y_delta += (screen_y - num_rows * ratio) * 0.5
+    elif y_ratio < x_ratio:
+        x_delta += (screen_x - num_cols * ratio) * 0.5
+
+    $Groups.scale.x = ratio
+    $Groups.scale.y = ratio
+
+    $Groups.position.x = x_delta
+    $Groups.position.y = y_delta
--- Scripts/WFCTile.gd
+++ Scripts/WFCTile.gd
@@ -7,3 +7,11 @@ var idx: int
 
 func _ready():
     set_name("Tile %d" % idx)
+
+
+func _draw():
+    var line_color = Color(1.0, 0.0, 0.0)
+    draw_line(Vector2(0, 0), Vector2(1, 0), line_color)
+    draw_line(Vector2(1, 0), Vector2(1, 1), line_color)
+    draw_line(Vector2(1, 1), Vector2(0, 1), line_color)
+    draw_line(Vector2(0, 1), Vector2(0, 0), line_color)
